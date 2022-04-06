56ccb7601c0f0d42d65dd4696ccef85cc10e91dd
Foysal <95936171+Onlygreat420@users.noreply.github.com>
2022-04-06 06:37:30 +0600

    Add files via upload

---
+++ hacking_NASA.js
@@ -0,0 +1,31 @@
+ numlist=[];
+  colors = ["red"];
+     for(let i=0;i<100;i++){
+       numlist.push(i+'%');
+     }
+     numlist.push('NASA hacked!');
+ var id = document.getElementById('great') ;
+ var load = document.querySelector('.load');
+function ArrayPlusDelay(array, delegate, delay) {
+  var i = 0;
+ 
+   // seed first call and store interval (to clear later)
+  var interval = setInterval(function() {
+    	// each loop, call passed in function
+      delegate(array[i]);
+      
+        // increment, and if we're past array, clear interval
+      if (i++ >= array.length - 1)
+          clearInterval(interval);
+  }, delay);
+  
+  return interval
+}
+ 
+var inter = ArrayPlusDelay(numlist, function(obj) {id.innerHTML=obj},80);
+
+var great = ArrayPlusDelay(colors, function(obj) {load.style.background=obj},8300);
+
+var loadbg = ArrayPlusDelay(colors, function(obj) {document.body.style.background=obj},7500);
+
+     
